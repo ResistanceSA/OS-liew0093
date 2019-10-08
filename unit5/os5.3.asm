8708ee23cab27fc9e95bef34dc8520e40d80bc4a
Vincent Liew <liew0093@flinders.edu.au>
2019-10-08 20:50:33 +1030

    5.3

--- unit5/os5.3.asm
+++ unit5/os5.3.asm
@@ -524,14 +524,18 @@ print_dhex: {
 resume_pdb: {
     .const pdb_number = 0
     .label p = stored_pdbs
+    .label __8 = $4f
     .label ss = $54
     .label i = $14
-    .label __17 = $56
-    .label __18 = $49
-    lda #0
+    .label __18 = $56
+    .label __19 = $49
+    lda p+OFFSET_STRUCT_PROCESS_DESCRIPTOR_BLOCK_STORAGE_START_ADDRESS
     sta.z dma_copy.src
+    lda p+OFFSET_STRUCT_PROCESS_DESCRIPTOR_BLOCK_STORAGE_START_ADDRESS+1
     sta.z dma_copy.src+1
+    lda p+OFFSET_STRUCT_PROCESS_DESCRIPTOR_BLOCK_STORAGE_START_ADDRESS+2
     sta.z dma_copy.src+2
+    lda p+OFFSET_STRUCT_PROCESS_DESCRIPTOR_BLOCK_STORAGE_START_ADDRESS+3
     sta.z dma_copy.src+3
     lda #<$3ff
     sta.z dma_copy.dest
@@ -546,13 +550,26 @@ resume_pdb: {
     lda #>$400
     sta.z dma_copy.length+1
     jsr dma_copy
-    lda #<$800
+    lda p+OFFSET_STRUCT_PROCESS_DESCRIPTOR_BLOCK_STORAGE_START_ADDRESS
+    sta.z __8
+    lda p+OFFSET_STRUCT_PROCESS_DESCRIPTOR_BLOCK_STORAGE_START_ADDRESS+1
+    sta.z __8+1
+    lda p+OFFSET_STRUCT_PROCESS_DESCRIPTOR_BLOCK_STORAGE_START_ADDRESS+2
+    sta.z __8+2
+    lda p+OFFSET_STRUCT_PROCESS_DESCRIPTOR_BLOCK_STORAGE_START_ADDRESS+3
+    sta.z __8+3
+    lda.z dma_copy.src
+    clc
+    adc #<$800
     sta.z dma_copy.src
-    lda #>$800
+    lda.z dma_copy.src+1
+    adc #>$800
     sta.z dma_copy.src+1
-    lda #<$800>>$10
+    lda.z dma_copy.src+2
+    adc #0
     sta.z dma_copy.src+2
-    lda #>$800>>$10
+    lda.z dma_copy.src+3
+    adc #0
     sta.z dma_copy.src+3
     lda #<$1fff
     sta.z dma_copy.dest
@@ -598,20 +615,20 @@ resume_pdb: {
     lda #<$d640
     clc
     adc.z i
-    sta.z __17
+    sta.z __18
     lda #>$d640
     adc.z i+1
-    sta.z __17+1
+    sta.z __18+1
     lda.z ss
     clc
     adc.z i
-    sta.z __18
+    sta.z __19
     lda.z ss+1
     adc.z i+1
-    sta.z __18+1
+    sta.z __19+1
     ldy #0
-    lda (__17),y
-    sta (__18),y
+    lda (__18),y
+    sta (__19),y
     inc.z i
     bne !+
     inc.z i+1
@@ -632,6 +649,7 @@ dma_copy: {
     .label __5 = $41
     .label __7 = $45
     .label __9 = $49
+    .label src = $4f
     .label list_request_format0a = $16
     .label list_source_mb_option80 = $17
     .label list_source_mb = $18
@@ -645,7 +663,6 @@ dma_copy: {
     .label list_dest_addr = $22
     .label list_dest_bank = $24
     .label list_modulo00 = $25
-    .label src = $4f
     .label dest = $b
     .label length = $14
     lda #0
