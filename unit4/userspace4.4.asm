9c454493a11f9f73608f0fd45fb46939ac317d81
Vincent Liew <liew0093@flinders.edu.au>
2019-10-09 14:14:20 +1030

    s

--- unit4/userspace4.4.asm
+++ unit4/userspace4.4.asm
@@ -5,13 +5,9 @@ main: {
     jsr print_string
   __b1:
     jmp __b1
-    string: .text "print via print string api"
-    .byte 0
 }
 print_string: {
     jsr enable_syscalls
-    lda main.string
-    sta $301
     jsr call_syscall02
     rts
 }
--- unit4/userspace4.4.vs
+++ unit4/userspace4.4.vs
@@ -1,7 +1,6 @@
-al C:845 .enable_syscalls
+al C:824 .enable_syscalls
 al C:810 .__b1
-al C:813 .string
-al C:83b .call_syscall02
-al C:82e .print_string
+al C:81a .call_syscall02
+al C:813 .print_string
 al C:80b .upstartEnd
 al C:80d .main
