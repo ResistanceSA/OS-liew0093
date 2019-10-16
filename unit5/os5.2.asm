32dd8a48df4c0f701aa07864ab25de6b5ad783c0
Vincent Liew <liew0093@flinders.edu.au>
2019-10-16 13:16:19 +1030

    home

--- unit5/os5.2.asm
+++ unit5/os5.2.asm
@@ -491,10 +491,10 @@ initialise_pdb: {
   Hypervisor saved state registers in Appendix D of the MEGA65 User's
   Guide. */
     ldy #7
-    lda #<$90d
+    lda #<$180d
     sta (ss),y
     iny
-    lda #>$90d
+    lda #>$180d
     sta (ss),y
     rts
   __b5:
