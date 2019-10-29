a71da56a558fe8c7d53b4e584cc5f2b198344a93
Vincent Liew <liew0093@flinders.edu.au>
2019-10-29 17:51:49 +1030

    unit7

--- unit7/wu7_solution.c
+++ unit7/wu7_solution.c
@@ -47,6 +47,7 @@ void wu7_examine_file_system(void)
   // Within that, find the partition entry that has a FAT32 partition in it (partition type
   // will be 0x0c), and then use extract_uint32() to get the start and size of the partition
   // into p_start and p_size.
+  //THIS IS CHECKPOINT 1 DO NOT EDIT
   for(int i = 446; i<= 494;i+=16){
       if(extract_uint32(i+0x4) == 0x0c){
       p_start =  extract_uint32(i+0x8);
@@ -60,7 +61,7 @@ void wu7_examine_file_system(void)
   // or simply reading bytes from sector_buffer[] to get the values for:
   sdcard_readsector(p_start);
   // f_sectors_per_fat, f_rootdir_cluster, f_reserved_sectors and f_sectors_per_cluster.
-  for(int i = 446; i<= 494;i+=16){
+  for(int i = 2048; i<=176079;i+=1){
     if(extract_uint32(i+0x4) == 0x0c){
   f_sectors_per_cluster = extract_uint32(i+0x010);
           f_sectors_per_fat = extract_uint32(i+0x21);
