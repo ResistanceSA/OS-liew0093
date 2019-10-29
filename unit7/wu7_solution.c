fae499955d9ab82d07d3384e2d5b9fad0f1f8c23
Vincent Liew <liew0093@flinders.edu.au>
2019-10-29 18:34:46 +1030

    unit7

--- unit7/wu7_solution.c
+++ unit7/wu7_solution.c
@@ -64,6 +64,7 @@ void wu7_examine_file_system(void)
   // f_sectors_per_fat, f_rootdir_cluster, f_reserved_sectors and f_sectors_per_cluster.
   sdcard_readsector(0);
   //  for(int i=0;i<4096;i++){
+ f_reserved_sectors = 2;
  for(int i = 446; i<= 494;i+=16){
       if(extract_uint32(i+0x4) == 0x0c){
 
