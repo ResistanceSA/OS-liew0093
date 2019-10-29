fb11478457e7731d41a85d958e54c986e02bc07a
Vincent Liew <liew0093@flinders.edu.au>
2019-10-29 16:48:00 +1030

    unit7

--- unit7/wu7_solution.c
+++ unit7/wu7_solution.c
@@ -60,8 +60,8 @@ void wu7_examine_file_system(void)
   // or simply reading bytes from sector_buffer[] to get the values for:
   sdcard_readsector(1);
   // f_sectors_per_fat, f_rootdir_cluster, f_reserved_sectors and f_sectors_per_cluster.
- for(int i = 446; i<= 494;i+=16){
-   if(extract_uint32(i+0x4) == 0x0c){
+  // for(int i = 446; i<= 494;i+=16){
+  //  if(extract_uint32(i+0x4) == 0x0c){
      f_sectors_per_fat = extract_uint32(i+0x21);
   f_rootdir_cluster = sector_buffer[0x02C + 4];
   f_reserved_sectors = sector_buffer[0x03+2];
