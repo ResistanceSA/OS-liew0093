badcf91d9a273b96a9e982ec0a51eb94309f876e
Vincent Liew <liew0093@flinders.edu.au>
2019-10-29 18:41:48 +1030

    unit7

--- unit7/wu7_solution.c
+++ unit7/wu7_solution.c
@@ -75,7 +75,7 @@ u_int32_t f_fat2_sector=0;
 u_int32_t f_rootdir_sector=0;
 u_int32_t f_rootdir_cluster=0;
       }
-
+   }
   // Then use those values to compute the values of f_fat1_Sector, f_fat2_sector, f_rootdir_sector
   // and f_clusters (this last one can be calculated simple as the number of sectors per fat multiplied
   // by the number of 32-bit values (i.e., 4 bytes long each) that can be packed into a 512 byte sector).
