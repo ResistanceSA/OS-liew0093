a4d92f1e85c472cf489d536b41c01bdba15cad44
Vincent Liew <liew0093@flinders.edu.au>
2019-10-29 15:59:10 +1030

    unit7

--- unit7/wu7_solution.c
+++ unit7/wu7_solution.c
@@ -47,7 +47,7 @@ void wu7_examine_file_system(void)
   // Within that, find the partition entry that has a FAT32 partition in it (partition type
   // will be 0x0c), and then use extract_uint32() to get the start and size of the partition
   // into p_start and p_size.
-  for(int i = 446; i<= 510;i+16){
+  for(int i = 446; i<= 494;i+16){
     if(extract_uint32(i) == 0x0c){
       p_start =  extract_uint32(i+0x8);
       p_size = extract_uint32(i+0xC);
