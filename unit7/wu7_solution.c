b25fb910d1c9d8ebbf3e33722e028a31e34ca1f4
Vincent Liew <liew0093@flinders.edu.au>
2019-10-29 18:45:44 +1030

    unit7

--- unit7/wu7_solution.c
+++ unit7/wu7_solution.c
@@ -42,14 +42,7 @@ void extract_filename(int offset, char *dest)
 
 void wu7_examine_file_system(void)
 {
-f_reserved_sectors= 2;
-u_int32_t f_sectors_per_fat=0;
-u_int32_t f_clusters=0;
-unsigned char f_sectors_per_cluster=0;
-u_int32_t f_fat1_sector= 101010;
-u_int32_t f_fat2_sector=0;
-u_int32_t f_rootdir_sector=0;
-u_int32_t f_rootdir_cluster=0;
+
   // XXX - First, read the Master Boot Record, which is in the first sector of the disk.
   sdcard_readsector(0);
   // Within that, find the partition entry that has a FAT32 partition in it (partition type
@@ -62,14 +55,6 @@ u_int32_t f_rootdir_cluster=0;
       p_size = extract_uint32(i+0xC);
       //  printf("%x",p_start & 0xff);
       // return;
-u_int32_t f_reserved_sectors= 2;
-u_int32_t f_sectors_per_fat=0;
-u_int32_t f_clusters=0;
-unsigned char f_sectors_per_cluster=0;
-u_int32_t f_fat1_sector= 101010;
-u_int32_t f_fat2_sector=0;
-u_int32_t f_rootdir_sector=0;
-u_int32_t f_rootdir_cluster=0;
     }
   }
   // Complexity guide: My solution was 6 lines long.
@@ -78,26 +63,26 @@ u_int32_t f_rootdir_cluster=0;
   // or simply reading bytes from sector_buffer[] to get the values fo:r
 
   // f_sectors_per_fat, f_rootdir_cluster, f_reserved_sectors and f_sectors_per_cluster.
+ f_reserved_sectors= 2;
+ f_sectors_per_fat=2;
+ f_clusters=1;
+ f_sectors_per_cluster=0;
+ f_fat1_sector= 101010;
+ f_fat2_sector=0;
+ f_rootdir_sector=0;
+ f_rootdir_cluster=0;
+
   sdcard_readsector(0);
    for(int i = 446; i<= 494;i+=16){
       if(extract_uint32(i+0x4) == 0x0c){
 	p_start =  extract_uint32(i+0x8);
-u_int32_t f_reserved_sectors= 2;
-u_int32_t f_sectors_per_fat=0;
-u_int32_t f_clusters=0;
-unsigned char f_sectors_per_cluster=0;
-u_int32_t f_fat1_sector= 101010;
-u_int32_t f_fat2_sector=0;
-u_int32_t f_rootdir_sector=0;
-u_int32_t f_rootdir_cluster=0;
+
       }
    }
   // Then use those values to compute the values of f_fat1_Sector, f_fat2_sector, f_rootdir_sector
   // and f_clusters (this last one can be calculated simple as the number of sectors per fat multiplied
   // by the number of 32-bit values (i.e., 4 bytes long each) that can be packed into a 512 byte sector).
   // Complexity guide: My solution was 11 lines long.
-
-
 
 }
 
