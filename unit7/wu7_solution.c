ede2e84e851d6e2e160d222748e64553d92c339f
Vincent Liew <liew0093@flinders.edu.au>
2019-10-30 12:27:28 +1030

    wut

--- unit7/wu7_solution.c
+++ unit7/wu7_solution.c
@@ -71,10 +71,10 @@ void wu7_examine_file_system(void)
   for(int i = 446; i<= 494;i+=16){
       if(extract_uint32(i+0x4) == 0x0c){
 	i = i+ 0x000;
-    /*char*/  f_reserved_sectors= extract_uint32(i+0x0e);
+    /*char*/  f_reserved_sectors= extract_uint32(i+0x08e);
   f_sectors_per_fat=extract_uint32(i+ 0x024);
-  f_rootdir_cluster= extract_uint32(p_start+0x02c);
-  f_sectors_per_cluster=(p_start+0x00d);
+  f_rootdir_cluster= extract_uint32(i+0x02c);
+  f_sectors_per_cluster=(i+0x00d);
  // f_clusters=1;
 
  f_fat1_sector= 0;
